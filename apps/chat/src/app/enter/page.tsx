59eee76f62d57d41749508918c15235d916b1b14
zhangpeng.eic <zhangpeng.eic@bytedance.com>
2023-05-19 21:39:52 +0800

    MOD: 修改typo

--- apps/chat/src/app/enter/page.tsx
+++ apps/chat/src/app/enter/page.tsx
@@ -1,30 +1,31 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 
 import styles from "@/app/login/login.module.scss";
 import Locales from "@/locales";
 
 export default function Index() {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <div className={styles["content"]}>
-      <h2 className={styles["text"]}>AI BOT</h2>
-      <div className={styles["buttons"]}>
-        <button
-          className={styles["button"]}
-          onClick={() => router.push("/login")}
-        >
-          {Locales.Index.Login}
-        </button>
-        <button
-          className={styles["button"]}
-          onClick={() => router.push("/register")}
-        >
-          {Locales.Index.Register}
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles["content"]}>
+            <h2 className={styles["text"]}>ChatGPT 3.5/4.0</h2>
+            <h4 className={styles["text"]}>除标准功能外，插件和联网功能开发中 敬请期待...</h4>
+            <div className={styles["buttons"]}>
+                <button
+                    className={styles["button"]}
+                    onClick={() => router.push("/login")}
+                >
+                    {Locales.Index.Login}
+                </button>
+                <button
+                    className={styles["button"]}
+                    onClick={() => router.push("/register")}
+                >
+                    {Locales.Index.Register}
+                </button>
+            </div>
+        </div>
+    );
 }
